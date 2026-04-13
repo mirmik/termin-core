1885d58f7a8b016929e07c254950814016409ff6
mirmik <mirmikns@yandex.ru>
2026-04-13 15:38:45 +0300

    rename termin as termin-app

--- termin-base/python/tcbase/__init__.py
+++ termin-base/python/tcbase/__init__.py
@@ -1,3 +1,7 @@
+from termin_nanobind.runtime import preload_sdk_libs
+
+preload_sdk_libs("termin_base")
+
 from tcbase._tcbase_native import *
 from tcbase._tcbase_native import log
 from tcbase.keys import Key
